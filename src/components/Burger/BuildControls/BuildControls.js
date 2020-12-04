ca0e7434c7ab8a29da3a6d368ce0f33583470e3b
Abhilash <amu@narola.email>
2020-12-04 16:47:35 +0530

    Adding authentication to burger_creation

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -18,13 +18,13 @@ const buildControls = (props) => (
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
                 removed={() => props.ingredientRemoved(ctrl.type)}
-                disabled={props.disabled.[ctrl.type]}
+                disabled={props.disabled[ctrl.type]}
             />
         ))  }
         <button 
         className={classes.OrderButton}
         disabled={!props.purchasable}
-        onClick={props.ordered}>ORDER NOW</button>
+        onClick={props.ordered}>{props.isAuth ? 'ORDER NOW' : 'SIGN UP TO ORDER'}</button>
     </div>
 );
 
